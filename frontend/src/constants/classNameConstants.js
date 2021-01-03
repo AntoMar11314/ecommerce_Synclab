fa469e26ce5d304c1004318116c72be1d950f01d
AntoMar11314 <marinoantonio885@gmail.com>
2021-01-03 20:15:02 +0100

    delete old project

--- frontend/src/constants/classNameConstants.js
+++
@@ -1,3 +0,0 @@
-export const HOME_SCREEN = 'HomeScreen';
-export const PRODUCT_SCREEN = 'ProductScreen';
-export const RELATED_STARS= 'RELATED_STARS';
--- frontend/src/utilities/changeImgsrc.js
+++
@@ -1,5 +0,0 @@
-export default function changeImgsrc(evt){
-    var ProductImg=document.getElementById("big-img");
-    ProductImg.src=evt.target.src
-}
- 
