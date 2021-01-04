ff54d2159de0e8fbe34936db239f7bc4ac28a844
AntoMar11314 <marinoantonio885@gmail.com>
2021-01-04 16:02:53 +0100

    cancellazione

--- frontend/src/constants/classNameConstants.js
+++
@@ -1,5 +0,0 @@
-export const HOME_SCREEN = 'HomeScreen';
-export const PRODUCT_SCREEN = 'ProductScreen';
-export const RELATED_STARS= 'RELATED_STARS';
-export const SELLER_SCREEN='SELLER_SCREEN;'
-export const SEARCH_SCREEN='SEARCH_SCREEN';
